8ffe378e74b14a5b1afb6c50e57e6872710f4471
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-10-01 12:34:02 +0000

    Revert "style: remove unused variable (#2991)"
    This reverts commit 079d143fbe3e8a3594b39235c76bc3305c6bf8c4.

--- graph/max_flow_with_ford_fulkerson_and_edmond_karp_algo.cpp
+++ graph/max_flow_with_ford_fulkerson_and_edmond_karp_algo.cpp
@@ -27,7 +27,8 @@ class Graph {
         visited.reset();
         std::queue<int> q;
         q.push(source);
-        while (q.empty() == false) {
+        bool is_path_found = false;
+        while (q.empty() == false && is_path_found == false) {
             int current_node = q.front();
             visited.set(current_node);
             q.pop();
