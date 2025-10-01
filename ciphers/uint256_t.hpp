a6d7da717c671c7ae8e2f7e6be5a92c43b410200
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2025-10-01 12:34:02 +0000

    Revert "style: resolve `-Wunused-parameter` (#2990)"
    This reverts commit 335034743918b94e05ae89f232d27fe181dfd56f.

--- ciphers/uint256_t.hpp
+++ ciphers/uint256_t.hpp
@@ -319,7 +319,7 @@ class uint256_t {
      * @brief operator -- (post-decrement)
      * @returns decremented value of this
      */
-    inline uint256_t operator--(int) {
+    inline uint256_t operator--(int p) {
         --*this;
         return *this;
     }
